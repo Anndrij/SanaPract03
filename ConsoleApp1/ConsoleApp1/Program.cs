92907c9b5b3294ecac89dbf9cc2e5c6084b2641f
Shevcuk Andrey <andrijsevcuk201@gmail.com>
2022-12-03 12:32:42 +0200

    Complite program

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      Console.OutputEncoding = Encoding.Unicode;
+double a, b, c, d,x;
+Console.Write("Введіть значення a= ");
+a=Convert.ToDouble( Console.ReadLine());
+Console.Write("Введіть значення b=");
+b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення c= ");
+c=Convert.ToDouble( Console.ReadLine());
+Console.Write("Введіть значення d=");
+d = Convert.ToDouble(Console.ReadLine());
+        x = first_p(a, b, c, d);
+        Console.WriteLine($"Значення x={x}");
+        second_p(a, b, c, d);
+        third_p(a, b, x);
+        forth_p(a, b, c);
+    }
+    public static double first_p(double a, double b, double c, double d)
+{
+        double x = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - (Math.Pow(a, 2) / Math.Pow(b, 2));
+        return x;
+}
+    public static void second_p(double a, double b, double c, double d)
+    {
+        double y = ((5 * (a + b) * (c - d)) / ((1 / 2.0) * c)) + Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a));
+        Console.WriteLine($"Значення y={y}");
+    }
+    public static void third_p(double a, double b, double x)
+    {
+        double z =((Math.Pow((Math.Pow(x,2)-2*x),3)-(4*(Math.Pow(x,4)+1)))*(1-b))/(5*a+3*b) ;
+        Console.WriteLine($"Значення z={z}");
+    }
+    public static void forth_p(double a, double b, double c)
+    {
+        double r = ((1/2.0+((3/4.0)*b)-7/5.0)/(3*c+1))+(1/(a-c));
+        Console.WriteLine($"Значення r={r}");
+    }
+
+}
+
+
+
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+double a, b,  x, n,m;
+Console.OutputEncoding = Encoding.Unicode;
+Console.Write("Введіть значення a= ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення b=");
+b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення x= ");
+x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення n=");
+n = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення m=");
+m = Convert.ToDouble(Console.ReadLine());
+double z1 = (((m - 1) * Math.Sqrt(m))-((n-1)* Math.Sqrt(n)))/(Math.Sqrt(Math.Pow(x,3)*n)+n*m+Math.Pow(m,2)-m);
+double z2=(Math.Sqrt(m)- Math.Sqrt(n))/m;
+double y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / 2) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, 1 / 2.0) * (x - b);
+Console.WriteLine($"Значення z1={z1}");
+Console.WriteLine($"Значення z2={z2}");
+Console.WriteLine($"Значення y={y}");
